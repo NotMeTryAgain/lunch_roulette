622a419fb39e2c5a6ca16d8fc41c28dfc5007cd0
Michael Hebert <michaelhebert@Michaels-MacBook-Pro.local>
2016-04-19 10:46:31 -0400

    checkingout master

--- spec/features/users/user_session_is_unique_spec.rb
+++ spec/features/users/user_session_is_unique_spec.rb
@@ -1,29 +1,28 @@
-# require "rails_helper"
-# feature "user sessions in unique" do
-#   let!(:user1) { FactoryGirl.create(:user) }
-#   let!(:user2) { FactoryGirl.create(:user) }
-#
-#   # As a logged in user
-#   # I only want to see my restaurants listed
-#   # Because this app is all about my tastes
-#
-#   # ACCEPTANCE CRITERIA
-#   # [ ] I must only see restaurants I have posted
-#   # [ ] Other users restaurants are not visible to me
-#
-#   scenario "session is unique" do
-#     login_as_user(user1)
-#     click_on "Add Eatery"
-#     fill_in "Name", with: "Zambino's"
-#     click_on "Submit"
-#     click_on "Sign Out"
-#     login_as_user(user2)
-#     click_on "Add Eatery"
-#     fill_in "Name", with: "TumYum"
-#     click_on "Submit"
-#     save_and_open_page
-#
-#     expect(page).to have_content "TumYum"
-#     expect(page).to_not have_content "Zambino's"
-#   end
-# end
+require "rails_helper"
+feature "user sessions in unique" do
+  let!(:user1) { FactoryGirl.create(:user) }
+  let!(:user2) { FactoryGirl.create(:user) }
+
+  # As a logged in user
+  # I only want to see my restaurants listed
+  # Because this app is all about my tastes
+
+  # ACCEPTANCE CRITERIA
+  # [ ] I must only see restaurants I have posted
+  # [ ] Other users restaurants are not visible to me
+
+  scenario "session is unique" do
+    login_as_user(user1)
+    click_on "Add Eatery"
+    fill_in "Name", with: "Zambino's"
+    click_on "Submit"
+    click_on "Sign Out"
+    login_as_user(user2)
+    click_on "Add Eatery"
+    fill_in "Name", with: "TumYum"
+    click_on "Submit"
+    binding.pry
+    expect(page).to have_content "TumYum"
+    expect(page).to_not have_content "Zambino's"
+  end
+end
