ef630ced111a45a22eea017afdf2a170c9b3ef2b
Michael Hebert <michaelhebert@Michaels-MacBook-Pro.local>
2016-04-28 09:15:45 -0400

    changed radius to 1000

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,7 +51,7 @@ function initMap() {
       map.setCenter(pos);
       service.nearbySearch({
         location: pos,
-        radius: 5000,
+        radius: 1000,
         types: ['restaurant', 'cafe']
       }, callback);
     }, function() {
