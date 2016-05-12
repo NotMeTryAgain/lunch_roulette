4c13a68cf31aaac5fa36d634c7dcd6b08db97f6d
Michael Hebert <michaelhebert@Michaels-MacBook-Pro.local>
2016-05-12 09:49:16 -0400

    added notice for boonies, cleaned up

--- spec/features/home/user_adds_favorite_spec.rb
+++ spec/features/home/user_adds_favorite_spec.rb
@@ -1,20 +1,24 @@
 require 'rails_helper'
 
-feature "User visits Lunch Roulette", js: true do
-  let!(:user1) { FactoryGirl.create(:user) }
-  let!(:restaurant1) { FactoryGirl.create(:restaurant) }
+feature "User plays Lunch Roulette", js: true do
+  # let!(:user1) { FactoryGirl.create(:user) }
+  # let!(:restaurant1) { FactoryGirl.create(:restaurant) }
 
   # As an authenticated user
-  # I want to visst Lunch Roulette
+  # I want to visit Lunch Roulette
   # So I can play
 
   # ACCEPTANCE CRITERIA
   # [ ] I see a map with my location
+  # [ ] If there are no restaurants in my immediate vicinity I am notified
   # [ ] There is a 'Play!' button
+  # [ ] When I click 'Play!' I am presented with a random restaurant within my vicinity
 
-  scenario "successfully adds restaurant" do
-    login_as_user(user1)
+  scenario "clicks Play!" do
+    visit root_path
+    click_on "Play!"
 
     expect(page).to have_css('div#map')
+    expect(page.current_path).to eq root_path
   end
 end
